05f3de1cf7a986ff69f99b2fed5f3936cab54011
dinnysanjay <160900894+dinnysanjay@users.noreply.github.com>
2024-04-11 14:53:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function shortcut(s1, s2) {
-  // your code here
+function shortcut(string1, string2) {
+    // Check if either of the input strings is empty
+    if (string1 === "" || string2 === "") {
+        return "";
+    }
+
+    // Return the initial letters of the strings
+    return string1[0] + string2[0];
 }
 
-// Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
+// Do not change the code below
+const string1 = prompt("Enter the first string.");
+const string2 = prompt("Enter the second string.");
+alert(shortcut(string1, string2));
